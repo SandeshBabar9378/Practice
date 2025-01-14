c719ff280c34757f597762c6b33b71e478a7f678
sandy9378 <sandesh9378@gmail.com>
2025-01-14 14:48:37 +0530

    Breeder detail page

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,14 +2,19 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {
+			extend: {
+				colors: {
+					'redish': '#f04336',
+					'blackish' :'#0a303a',
+				},
 			
 			keyframes: {
 				wiggle: {
 				  '0%, 100%': { transform: 'rotate(-15deg)' },
 				  '50%': { transform: 'rotate(15deg)' },
 				}
-			  }
+			
+			  },
 
 		},
 	},
